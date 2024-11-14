3d967c7ae69083705de200798bb6b644e79e466b
Rocio <ro.tournour@gmail.com>
2024-11-14 14:30:59 -0300

    feat(app.module): added DeathReportModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HealthModule } from "@/app/health/health.module";
 import { LoggerModule } from "@/shared/logger/logger.module";
 
 import { PharmacyModule } from "@/contexts/pharmacies/pharmacy.module";
+import { DeathReportModule } from "@/contexts/deathReports/death-reports.module";
 import { PostModule } from "@/contexts/posts/post.module";
 import { PostSectionModule } from "@/contexts/postSections/post-section.module";
 import { RainCityModule } from "@/contexts/rainCities/rain-city.module";
@@ -24,6 +25,7 @@ import { RainCityModule } from "@/contexts/rainCities/rain-city.module";
     PharmacyModule,
     PharmacyScheduleModule,
     RainCityModule,
+    DeathReportModule,
   ],
 })
 export class AppModule {}
