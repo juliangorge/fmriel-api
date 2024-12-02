ae537b1ddef893d6d4a4f6990d6b98aff63e641c
juliangorge <juliangorge@hotmail.com>
2024-12-02 15:42:44 -0300

    fix: added validation decorators to SingInDto

--- src/app/auth/auth.dto.ts
+++ src/app/auth/auth.dto.ts
@@ -1,15 +1,18 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty } from "class-validator";
 
 export class SignInDto {
   @ApiProperty({
     description: "Email address",
     example: "joedoe@gmail.com",
   })
+  @IsEmail()
   email!: string;
 
   @ApiProperty({
     description: "Password",
     example: "strongPassword123",
   })
+  @IsNotEmpty()
   password!: string;
 }
